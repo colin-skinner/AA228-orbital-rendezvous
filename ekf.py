0a084138964d917fabbf3d5839024f87b49d8d6c
Colin Skinner <colinaskinner@gmail.com>
2025-12-03 21:11:15 -0800

    commenting some ekf stuff

--- ekf.py
+++ ekf.py
@@ -3,7 +3,22 @@ from dataclasses import dataclass
 from collections.abc import Callable
 
 @dataclass
+class State:
+    x: np.ndarray  # Mean vector
+    P: np.ndarray  # Covariance matrix
+
+@dataclass
 class LinearEkf:
+    """ 
+    ## Args:
+    - **F** (Function[[np.ndarray], np.ndarray]): State Transition Matrix
+    - **U** (Function[[np.ndarray], np.ndarray]): Input Matrix
+    - **H** (Function[[np.ndarray], np.ndarray]): Observation Matrix
+    - **Q** (np.ndarray): Process Noise Covariance. Typically a diagonal matrix where each elements
+        the (expected) covariance of each element of the state
+    - **R** (np.ndarray): Measurement Noise Covariance. Typically a diagonal matrix where each elements
+        the (expected) covariance of each element of the measurement vector
+    """
     F: np.ndarray
     B: np.ndarray
     H: np.ndarray
@@ -12,6 +27,22 @@ class LinearEkf:
 
 @dataclass
 class NonlinearEkf:
+    """ 
+    ## Args:
+    - **get_f** (Function[[np.ndarray, np.ndarray], np.ndarray]): State Transition function
+        [ get_f(state, input) --> next_state ]
+    - **get_F** (Function[[np.ndarray], np.ndarray]): State Transition Covariance function
+        [ get_F(state, input) --> F ]
+    - **get_h** (Function[[np.ndarray, np.ndarray], np.ndarray]): Observation function
+        (turns state into what theoretical measurements it would create in that state)
+        [ get_h(state) --> meas_theoretical ]
+    - **get_H** (Function[[np.ndarray], np.ndarray]): Observation Covariance function
+        [ get_H(state) --> H ]
+    - **Q** (np.ndarray): Process Noise Covariance. Typically a diagonal matrix where each elements
+        the (expected) covariance of each element of the state
+    - **R** (np.ndarray): Measurement Noise Covariance. Typically a diagonal matrix where each elements
+        the (expected) covariance of each element of the measurement vector
+    """
     f: Callable[[np.ndarray, np.ndarray], np.ndarray]
     F: Callable[[np.ndarray], np.ndarray]
     h: Callable[[np.ndarray, np.ndarray], np.ndarray]
@@ -19,10 +50,6 @@ class NonlinearEkf:
     Q: np.ndarray
     R: np.ndarray
 
-@dataclass
-class State:
-    x: np.ndarray  # Mean vector
-    P: np.ndarray  # Covariance matrix
 
 def run_linear_ekf(state: State, ekf: LinearEkf, u: np.ndarray, z: np.ndarray):
     F, B, H, Q, R = ekf.F, ekf.B, ekf.H, ekf.Q, ekf.R
@@ -40,9 +67,7 @@ def run_linear_ekf(state: State, ekf: LinearEkf, u: np.ndarray, z: np.ndarray):
     S = H @ P_est @ H.T + R
     K = P_est @ H.T @ np.linalg.inv(S)
     x_next = x_est + K @ y_err
-    P_next = (np.eye(dim) - K@H) @ P_est
-    # P_next = (np.eye(dim) - K@H) @ P_est @ (np.eye(dim) - K@H).T + K@R@K.T # Joseph form (not used)
-
+    P_next = (np.eye(dim) - K@H) @ P_est @ (np.eye(dim) - K@H).T + K@R@K.T # Joseph form (not used)
     return State(x_next, P_next)
 
 def run_nonlinear_ekf(state: State, ekf: NonlinearEkf, u: np.ndarray, z: np.ndarray):
@@ -61,8 +86,5 @@ def run_nonlinear_ekf(state: State, ekf: NonlinearEkf, u: np.ndarray, z: np.ndar
     S = H @ P_est @ H.T + R
     K = P_est @ H.T @ np.linalg.inv(S)
     x_next = x_est + K @ y_err
-    P_next = (np.eye(dim) - K@H) @ P_est 
-    # P_next = (np.eye(dim) - K@H) @ P_est @ (np.eye(dim) - K@H).T + K@R@K.T # Joseph form (not used)
-
+    P_next = (np.eye(dim) - K@H) @ P_est @ (np.eye(dim) - K@H).T + K@R@K.T # Joseph form (not used)
     return State(x_next, P_next)
-
